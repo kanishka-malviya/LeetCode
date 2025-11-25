b322f5ec4ff81499d9cac46740696fa28773cd4e
Kanishka Malviya <86570138+kanishka-malviya@users.noreply.github.com>
2025-11-26 02:45:11 +0530

    Time: 50 ms (67.52%), Space: 56.8 MB (85.42%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    let counter = 0;
+
+    while (fast !== null && fast.next!== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            slow = head;
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+
+    return null;
+};
