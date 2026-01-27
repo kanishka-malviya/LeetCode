ee4f15f6964c212d1236fca87e8a447a6f64f941
Kanishka Malviya <86570138+kanishka-malviya@users.noreply.github.com>
2026-01-27 22:22:09 +0530

    Time: 6 ms (40.41%), Space: 61.2 MB (45.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var romanToIntMap = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
+    var result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (romanToIntMap[s[i]] < romanToIntMap[s[i + 1]] && i + 1 < s.length) {
+            result -= romanToIntMap[s[i]];
+        } else {
+            result += romanToIntMap[s[i]];
+        }
+    }
+
+    return result;    
+};
